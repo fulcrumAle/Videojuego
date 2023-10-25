5c9d70006db6e3f514149e28feb7d779363e82f0
fulcrumAle <134450730+fulcrumAle@users.noreply.github.com>
2023-10-25 09:45:04 -0400

    Subiendo videojuegos controller

---
+++ Videojuego - VideojuegosController/Models/Email.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VideoJuego.Models
+{
+    public class Email
+    {
+        [Key]
+        public int idEmail { get; set; }
+        public string E_mail { get; set; }
+        public int idUsuario { get; set; }
+        
+    }
+}
---
+++ Videojuego - VideojuegosController/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VideoJuego.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int idUsuario { get; set; }
+        public string Nombre { get; set; }
+        public bool Genero { get; set; }
+        public int Edad { get; set; }
+       
+    }
+}
