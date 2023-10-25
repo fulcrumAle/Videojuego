c139f350efeafdafca9787aaee2be62b35751947
fulcrumAle <134450730+fulcrumAle@users.noreply.github.com>
2023-10-25 09:35:42 -0400

    subiendo usuarios controller

---
+++ Videojuego - copia/Context/AplicacionContexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Videojuego.Models;
+
+namespace practicadetaller.Context
+{
+    public class AplicacionContexto : DbContext
+    {
+        public AplicacionContexto(DbContextOptions<AplicacionContexto> options) : base(options) { }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Email> Email { get; set; }
+        public DbSet<Videojuegos> Videojuegos { get; set; }
+    }
+}
